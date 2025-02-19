0d8d1a52a3f1e58e8894bf92c487d67c341a42d0
Mathilde Pind Rasmussen <mathilde@mathildepind.com>
2025-02-19 13:03:12 -0800

    create and add pressable delete button

--- App.tsx
+++ App.tsx
@@ -1,13 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Alert, Text, TouchableOpacity, View } from "react-native";
+import { theme } from "./theme";
 
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert(
+      "Are you sure you want to delete this?",
+      "It will be gone for good",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("OK, deleting"),
+          style: "destructive",
+        },
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+      ],
+    );
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
-        <Text>Coffee</Text>
-      </View>
-      <View style={styles.itemContainer}>
-        <Text>Tea</Text>
+        <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity
+          onPress={handleDelete}
+          style={styles.button}
+          activeOpacity={0.8}
+        >
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -16,17 +39,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: theme.colorWhite,
     justifyContent: "center",
   },
   itemContainer: {
     paddingVertical: 16,
     paddingHorizontal: 8,
-    borderBottomColor: "#1a759f",
+    borderBottomColor: theme.colorCerulean,
     borderBottomWidth: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   itemText: {
     fontSize: 18,
     fontWeight: "200",
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
   },
 });
